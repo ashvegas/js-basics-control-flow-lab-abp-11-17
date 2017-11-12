68965d18c2750777efc16dfb312112b1f159f961
AC <msashvegas@gmail.com>
2017-11-12 23:20:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function scuberGreetingForFeet(var){
-  if (var<400) {
+function scuberGreetingForFeet(value){
+  if (value === <400) {
   return "This one is on me!"
 }
 else if (var > 2000) {
   return "I will gladly take your thirty bucks."
 }
 else if {
-  
+
 }
-  
+
 }
