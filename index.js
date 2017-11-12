f8e4bfd0f8ed029314e900d770407e6eb3f842e5
AC <msashvegas@gmail.com>
2017-11-12 23:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
 function scuberGreetingForFeet(value){
-  if (value <400) {
+  if (value <=400) {
   return "This one is on me!"
 }
-else if (var > 2000) {
-  return "I will gladly take your thirty bucks."
-}
-else if {
-
-}
-
 }
