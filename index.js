9e9c22398fff8b293476c86da31c08fd7f0e2888
AC <msashvegas@gmail.com>
2017-11-12 23:55:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function switchOnCharmFromTip(tip){
   switch (tip){
   case 'generous':
     return "Thank you so much.";
+    break;
   case "not as generous":
     return "Thank you."
   default:
